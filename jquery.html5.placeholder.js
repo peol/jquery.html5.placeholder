346060d1fe013b8583acabde7484f169daaf0fdc
Andrée Hansson <peolanha@gmail.com>
2010-09-08 21:55:03 +0200

    Initial commit, not very thoroughly tested yet. Firefox3.*, Opera 10.*

---
+++ jquery.html5.placeholder.js
@@ -0,0 +1,50 @@
+/*
+ * HTML5 Placeholder Support through jQuery.
+ * Approach inspired (and partly taken) from Mike Taylors plugin over at
+ * http://github.com/miketaylr/jQuery-html5-placeholder
+ * 
+ * Doesn't use the input's real value as placeholder since that would allow .value to be
+ * the placeholder value (like most other approaches will fail).
+ */
+
+!function() {
+	var nativeSupport = "placeholder" in document.createElement('input');
+
+	$.fn.placeholder = function() {
+		return nativeSupport ? this : this.each(function() {
+			var
+				me     = $(this),
+				id     = me[0].id || "placeholder" + +new Date,
+				value  = me.val(),
+				offset = me.offset(),
+				label  = $('<label/>');
+
+			me
+				.attr('id', id)
+				.bind('focus blur', function(e) {
+					e.type == 'focus' && label.hide();
+					e.type == 'blur'  && me.val() == "" && label.show();
+				});
+
+			label
+				.css({
+					display:    value == "" ? 'block' : 'none',
+					color:      '#999',
+					position:   'absolute',
+					overflow:   'hidden',
+					top:        offset.top,
+					left:       offset.left,
+					width:      me.outerWidth(),
+					height:     me.outerHeight(),
+					marginTop:  5,
+					marginLeft: 5,
+					cursor:     me.css('cursor'),
+					lineHeight: me.css('line-height'),
+					fontSize:   me.css('font-size')
+				})
+				.attr('for', id)
+				.text( me.attr('placeholder') )
+				.appendTo('body');
+		});
+	};
+}();
